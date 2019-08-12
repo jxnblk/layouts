05f5e80801ba464291b2cdbf388872762394e6aa
Brent Jackson <jxnblk@gmail.com>
2019-08-12 16:23:31 -0400

    Edit code

--- src/code.js
+++ src/code.js
@@ -97,7 +97,7 @@ const renderCSSModules = (type, props, children) => {
 
 export const jsx = (type, props = {}, ...children) => {
   const { mode } = useContext()
-  const { depth = 0 } = props
+  const { depth = 0 } = props || {}
   const name = type.displayName || type
   const tag = elements[mode][name]
 
