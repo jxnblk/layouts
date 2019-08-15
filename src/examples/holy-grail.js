e99f01d09a1ae64b3b65fe2672938b39a8cc6634
Matt Rothenberg <mrothenberg@heroku.com>
2019-08-15 16:57:09 -0400

    Set explicit minWidth on main content div for Holy Grail

--- src/examples/holy-grail.js
+++ src/examples/holy-grail.js
@@ -5,7 +5,7 @@ export default () => (
   <Flex sx={{ flexDirection: "column", minHeight: "100vh" }}>
     <Box>Header</Box>
     <Flex sx={{ flex: 1, flexDirection: ["column", "row"] }}>
-      <Box sx={{ flex: 1 }}>Main Content</Box>
+      <Box sx={{ flex: 1, minWidth: 0 }}>Main Content</Box>
       <Box sx={{ flexBasis: ["auto", 64], order: -1 }}>Nav</Box>
       <Box sx={{ flexBasis: ["auto", 64] }}>Ads</Box>
     </Flex>
